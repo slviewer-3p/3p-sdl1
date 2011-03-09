fde43e51e05ada6565f2c2c13d1a566630470049
Andrew de Laix <alain@lindenlab.com>
2011-03-09 20:19:54 +0000

    fully working build and packaging on linux.

--- build-cmd.sh
+++ build-cmd.sh
@@ -9,6 +9,9 @@ TOP="$(dirname "$0")"
 
 SDL_VERSION="1.2.14"
 SDL_SOURCE_DIR="SDL-$SDL_VERSION"
+DIRECTFB_VERSION="1.4.9"
+DIRECTFB_SOURCE_DIR="DirectFB-$DIRECTFB_VERSION"
+
 
 if [ -z "$AUTOBUILD" ] ; then 
     fail
@@ -26,21 +29,28 @@ set -x
 stage="$(pwd)"
 case "$AUTOBUILD_PLATFORM" in
     "linux")
+        pushd "$TOP/$DIRECTFB_SOURCE_DIR"
+            LDFLAGS="-m32" CFLAGS="-m32" CXXFLAGS="-m32" ./configure --prefix="$stage"
+            make
+            make install
+        popd
         pushd "$TOP/$SDL_SOURCE_DIR"
             LDFLAGS="-m32  -L"$stage/lib"" CFLAGS="-m32" CXXFLAGS="-m32" ./configure --prefix="$stage" --target=i686-linux-gnu
             make
             make install
         popd
+        mv lib release
+        mkdir -p lib
+        mv release lib
     ;;
     *)
         exit -1
     ;;
 esac
 
-# cp -r "$TOP/$SDL_SOURCE_DIR/include" "$stage"
 
-# mkdir -p "$stage/LICENSES"
-# cp "$TOP/$OPENAL_SOURCE_DIR/COPYING" "$stage/LICENSES/openal.txt"
+mkdir -p "$stage/LICENSES"
+cp "$TOP/$SDL_SOURCE_DIR/COPYING" "$stage/LICENSES/SDL.txt"
 
 pass
 
